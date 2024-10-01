3a9efb28b256d2ee26afa593753f5dfad80e2bd3
Jinnie <hjk7639@gmail.com>
2024-10-01 19:41:28 +0900

    Add: Type inference Around useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import type { Place } from './api/Place';
+import { useState } from 'react';
 import Map from './components/map';
 import LocationSearch from './components/locationSearch';
 
 function App() {
+  const [place, setPlace] = useState<Place | null>(null);
+
   return (
     <div className="h-screen w-screen grid grid-cols-12">
       <div className="col-span-3 p-2">
         <LocationSearch />
       </div>
       <div className="col-span-9">
-        <Map />
+        <Map place={place} />
       </div>
     </div>
   );
