7f45bb752f544f48edeb9f8095a7020f3388700e
Jinnie <hjk7639@gmail.com>
2024-10-01 19:19:55 +0900

    Add: Map, LocationSearch components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Map from './components/map';
+import LocationSearch from './components/locationSearch';
+
 function App() {
-  return <div className="m-4">Hi there~</div>;
+  return (
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
+  );
 }
 
 export default App;
