b32aebd183a9634b50579a84a24e3dd1759578bc
Jinnie <hjk7639@gmail.com>
2024-10-02 20:15:11 +0900

    Add: Form Submissions with TS

--- src/components/locationSearch.tsx
+++ src/components/locationSearch.tsx
@@ -7,5 +7,26 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [places, setPlaces] = useState<Place[]>([]);
-  return <div>Search for a Location</div>;
+  const [term, setTerm] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('NEed to search api', term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold">Search</label>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md shadow-sm focus: border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
